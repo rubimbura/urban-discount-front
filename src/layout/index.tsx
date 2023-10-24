a3189c3686a30a98bb789ed9f7db2e43bd48dbe7
brubimbura <brubimbura@bk.rw>
2023-10-24 18:09:24 +0200

    latest

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,9 @@
 import { Suspense } from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import { lazy } from 'react'
-import DefaultLayout from './DefaultLayout'
 
 const LandingPage = lazy(() => import('../pages/landingPage')) 
-const Login = lazy(() => import('../pages/auth/Login'))
+
 
 const AppLayout = () => {
   return(
@@ -12,9 +11,6 @@ const AppLayout = () => {
       <Suspense fallback={<></>}>
         <Routes>
           <Route path="/" element={<LandingPage/>}/>
-          <Route path="/admin/login" element={<Login/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="*" element={<DefaultLayout/>}/>
         </Routes>
       </Suspense>
     </BrowserRouter>
